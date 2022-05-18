0449a3072cbe834ac21b9dc873166812f833a1ef
Aaryaman sharma <er.aaryamansharma09@gmail.com>
2022-05-18 21:08:31 +0530

    Assignment_1_string

--- Assignment_1_string/Question_1.py
+++ Assignment_1_string/Question_1.py
@@ -1,7 +1,7 @@
 
 # Python program to capitalize the first and last character of each word in a string (input string should be a statement)
 
-def String_Lettercap_f_and_a(str):
+def String_Lettercap_f_and_l(str):
       
 
     return ' '.join(map(lambda s: s[:-1]+s[-1].upper(), 
@@ -9,4 +9,4 @@ def String_Lettercap_f_and_a(str):
       
 s = input("Enter string : ")
 print("String before:", s)
-print("String after:", String_Lettercap_f_and_a(str))
+print("String after:", String_Lettercap_f_and_l(str))
